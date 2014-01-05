e941a85d5e1631affbebb54b425c6cfb9111e47f
johnnynonumber <johnnynonumber@users.noreply.github.com>
2014-01-05 14:13:46 +0100

    Update loremaster.py
    Big merge of code to get a list of reddits, check them for recent activity, then check for keywords

--- loremaster.py
+++ loremaster.py
@@ -1,36 +1,89 @@
-import praw, time
+import praw, time, datetime, urllib2, re
+from datetime import datetime
+from pprint import pprint
 
 r = praw.Reddit(user_agent='u/JohnnyNoNumber testing PRAW')
+
+# Scrape source of a website that lists many subreddits
+# Create a text file with all the subreddits listed
+# If necessary, remove duplicated listings so each subreddit is a unique entry
+
+pagenumbers = range(1,2)
+redditlist = []
+
+print "\n","Scraping redditlist.com for top 5000 subreddits, and saving to all_reddits.txt..."
+
+for page in pagenumbers:
+        url = 'http://www.redditlist.com/page-'+str(page)+''
+        source = urllib2.urlopen(url).read()
+        find_group = re.compile('">(.+?)</a></td>')
+        subreddit_per_page = find_group.findall(source)
+        for subreddit in subreddit_per_page:
+                if subreddit not in redditlist:
+                        redditlist.append(subreddit)
+        #print "Page",page,"done!" 
+        
+print "Scraping done!"
+print "\n","Writing subreddits to file..."       
+with open("all_reddits.txt", mode='wt') as myfile:
+        myfile.write('\n'.join(redditlist))
+        
+print "Done writing to file!"
+print "--------------------------"
+
+
+# Login to reddit, and get the most recent post of every subreddit in all_reddits.txt
+# If the post was made in 2014, consider that subreddit "fresh"
+# Build a list of fresh subreddits, and save to a text file
 
 print "\n","Logging into Reddit...","\n"
 r.login(username='johnnynonumber',password='toasters8')
 
 if r.is_logged_in() is True:
         print "Logged in.","\n"
-else:
-        print "Nooooo!"
 
-already_done = []        
-my_keywords = open("tags.txt").read().split("\n")
-subreddit_list = open("subreddit_list.txt").read().split("\n")
+subreddit_list = open("all_reddits.txt").read().split("\n")
+fresh_subreddits = []
+
+print "Checking for date of submission for most recent post in every subreddit..."
+print "If the post was created in 2014, put that subreddit to a 'fresh' list."
+print "This may take some time..."
+
+for subreddit in subreddit_list: 
+    target_subreddit = r.get_subreddit(subreddit)
+    for post in target_subreddit.get_new(limit=1):
+        newest_post = r.get_submission(submission_id = post.id)
+        if newest_post.created_utc >= 1388534400:
+                fresh_subreddits.append(target_subreddit.display_name)
+
+with open("fresh_subreddits.txt", mode='wt') as myfile:
+        myfile.write('\n'.join(fresh_subreddits))
+
+
+
+# Now check all the fresh subreddits for keywords
+
 loop_count = 0
-
+already_done = []
+core_list = open("fresh_subreddits.txt").read().split("\n")        
+my_keywords = open("tags.txt").read().split("\n")
+start_time = str(datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
 
 while True:
-    for subreddit in subreddit_list: 
-        target_subreddit = r.get_subreddit(subreddit)
-        for post in target_subreddit.get_hot(limit=10):
-            op_text = post.selftext.lower()
-            has_my_keywords = any(string in op_text for string in my_keywords)
+    for core in core_list:
+        core_subreddit = r.get_subreddit(core)
+        print "Checking",core_subreddit
+        for post in core_subreddit.get_hot(limit=10):
+            op_title = post.title
+            has_my_keywords = any(string in op_title for string in my_keywords)
             if post.id not in already_done and has_my_keywords:
                 already_done.append(post.id)
-                msg = '[NoNumber Bot] Post of Interest in r/%s (%s)' % (target_subreddit, post.short_link)
-                r.user.send_message('JohnnyNoNumber', msg)
-                print "Found a post in the subreddit:",subreddit
+                #msg = '[NoNumber Bot] Post of Interest in r/%s (%s)' % (core_subreddit, post.short_link)
+                #r.user.send_message('JohnnyNoNumber', msg)
+                print "\n","Found a post in the subreddit:",core_subreddit,"\n"
 
-    print "Waiting 2 minutes.","\n\n"
     loop_count += 1
-    #with open("redditlist.txt", mode='wt') as myfile:
-    #myfile.write('\n'.join(redditlist))
-    time.sleep(120)
-    print "Checking subreddit_list"
+    print "\n","Checked",loop_count,"times since",start_time,"UTC"
+    print "\n","Waiting 5 seconds before next check.","\n\n"
+    time.sleep(5)
+    print "Checking subreddit_list","\n"
