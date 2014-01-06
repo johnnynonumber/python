98176131b783d33bbbfcddbfc49dab1af235a1d7
johnnynonumber <johnnynonumber@users.noreply.github.com>
2014-01-06 17:20:21 +0100

    update loremaster.py

--- loremaster.py
+++ loremaster.py
@@ -1,3 +1,4 @@
+import os.path as path
 import urllib2
 import re
 import time
@@ -6,47 +7,63 @@ from datetime import datetime
 import praw
 from requests.exceptions import HTTPError
 
-
+def modification_date(filename):
+    t = path.getmtime(filename)
+    return datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
 
 def Scrape():
     # Scrape source of a website that lists many subreddits
     # Build a list of all the subreddits, all_reddits
     # If necessary, remove duplicated listings so each subreddit is a unique entry
-    pagenumbers = range(1,2)
     all_reddits = []
-    print "\n","Scraping redditlist.com for top 5000 subreddits..."
-    for page in pagenumbers:
-        url = 'http://www.redditlist.com/page-'+str(page)+''
-        source = urllib2.urlopen(url).read()
-        find_group = re.compile('">(.+?)</a></td>')
-        subreddit_per_page = find_group.findall(source)
-        for subreddit in subreddit_per_page:
-            if subreddit not in all_reddits:
-                all_reddits.append(subreddit)
+    print "\n","Scraping http://www.reddit.com/r/gaming/wiki/faq for gaming reddits..."
+    url = 'http://www.reddit.com/r/gaming/wiki/faq'
+    source = urllib2.urlopen(url).read()
+    find_group = re.compile('<a href="/r/(.+?)"')
+    subfound = find_group.findall(source)
+    for subreddit in subfound:
+        if subreddit not in all_reddits:
+            all_reddits.append(subreddit)
     print "Scraping done!","\n"
     return all_reddits;
 
 def FreshCheck(n):
-    # Check a list of reddits for freshness (most recent post made in 2014)
+    # Check a list of reddits for freshness (latest post < 1 week old)
     # Build/Update a list of fresh subreddits
     fresh_subreddits = []
     print "Checking",len(n),"subreddits for freshness. This may take some time...","\n"
-    for subreddit in n:
-        try: 
-            print subreddit
-            target_subreddit = r.get_subreddit(subreddit, fetch=True)
-            for post in target_subreddit.get_new(limit=1):
-                newest_post = r.get_submission(submission_id = post.id)
-                if newest_post.created_utc >= 1388534400:
-                    fresh_subreddits.append(target_subreddit.display_name)
+    for x in n:
+        try:
+            target_subreddit = r.get_subreddit(x, fetch=True)
+            avg_age = [] 
+            for post in target_subreddit.get_new(limit=10):
+                new_post = r.get_submission(submission_id = post.id)
+                avg_age.append(new_post.created_utc)
+            FreshPost(avg_age)
+            if FreshPost(avg_age):
+                fresh_subreddits.append(target_subreddit.display_name)
+            else: 
+                continue
         except HTTPError:
-            print "!! ",subreddit,"gave an error and was not checked !!"
+            print "!! ",x,"gave an error and was not checked !!"
         except praw.errors.InvalidSubreddit:
-            print "!! ",subreddit,"is not an existing subreddit and was not checked !!"
+            print "!! ",x,"is not an existing subreddit and was not checked !!"
+        except praw.errors.RedirectException:
+            print "!! ",x,"tried to redirect. Probably not a subreddit link !!" 
             continue
     print "\n","All subreddits checked for freshness.","\n"
+    print "Creating new list with",len(fresh_subreddits),"fresh subreddits..."
+    with open('gaming.txt','w') as create:
+        create.write("\n".join(fresh_subreddits))
     return fresh_subreddits;
-    
+ 
+def FreshPost(i):
+    t = time.time()
+    avg = sum(i) / float(len(i))
+    diff = t - avg
+    if diff < 604800:
+        return True;
+
 def KeywordCheck(n):
     # Check all the fresh subreddits for keywords
     loop_count = 0
@@ -54,7 +71,7 @@ def KeywordCheck(n):
     my_keywords = open("tags.txt").read().split("\n")
     start_time = str(datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
 
-    print "\n","Checking",len(n),"fresh subreddits for keywords..."
+    print "\n","Checking",len(n),"fresh subreddits for keywords...","\n"
     while True:
         for fresh in n:
             fresh_subreddit = r.get_subreddit(fresh)
@@ -86,13 +103,16 @@ if r.is_logged_in() is True:
 # Check if a file of fresh subreddits was already created
 print "Checking for previously fresh subreddits..."
 try:
-    f = open("master_fresh.txt").readlines()
+    f = open("gaming.txt").readlines()
     # If there is a file, check if it has entries
     # If it has entries, pass the contents to Freshcheck(), and pass that output into KeywordCheck()
     if f:
         print "A list is available!","\n"
-        master_fresh = open("master_fresh.txt").read().split("\n")
-        KeywordCheck(FreshCheck(master_fresh));
+        d = modification_date('gaming.txt')
+        print "List last modified:",d
+        gaming = open("gaming.txt").read().split("\n")
+        FreshCheck(gaming);
+        #KeywordCheck(FreshCheck(gaming));
     # If the file is empty...
     else:
         print "File found, but it has no entries. Getting new list..."
