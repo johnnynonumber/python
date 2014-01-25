b60cdf7c0482cc1ff278a40acaa8f8fad823d962
johnnynonumber <johnnynonumber@users.noreply.github.com>
2014-01-25 22:57:54 +0100

    Update imageget.py

--- imageget.py
+++ imageget.py
@@ -1,16 +1,11 @@
-import urllib
-import urlparse
-import re
-import time
-
 import ytgets
 
-
+print "\n"
 youtubeuser = raw_input('Type the channel name and press Enter: --> ')
 channel_link = 'http://www.youtube.com/user/'+ youtubeuser +'/'
 
-if not ytgets.doesExist(channel_link):
+if not ytgets.does_exist(channel_link):
     print "Doesn't look like a channel."
 else:
     print "\n","Looks good! - Checking for images now!","\n"
-    ytgets.getVideos(ytgets.getPlaylists(channel_link))
+    ytgets.get_videos(ytgets.get_playlists(channel_link))
