cdae79a38f7fdab13fa0a75d2dde3f193b41da15
johnnynonumber <johnnynonumber@users.noreply.github.com>
2014-01-25 13:50:12 +0100

    Update imageget.py

--- imageget.py
+++ imageget.py
@@ -3,48 +3,80 @@ import urlparse
 import re
 import time
 
-def getPlaylists(channel):
-	# Get a list of playlists
-	channel_page = urllib.urlopen(channel)
-	channel_page_source = channel_page.read()
+def doesExist(channel_link):
+        '''
+        Takes a YouTube channel link, checks for the "empty channel" message
+        Returns True if message isn't found
+        ''' 
+        i = urllib.urlopen(channel_link).read()
+        regex = '<div class="channel-empty-message banner-message">'
+        check = re.findall(regex,i)
+        if not check:
+                return True;
 
-	playlist_regex = 'data-context-item-id="(.+?)"'
-	get_playlist_ID = re.findall(playlist_regex,channel_page_source)
-	return get_playlist_ID;
+def getPlaylists(channel_link):
+        '''
+        Takes a channel link and forms the channel playlist gallery URL
+        Scrapes the gallery and returns playlist IDs
+        '''
+        playlist_gallery = ''+ channel_link + 'videos?view=1'''
+
+        playlist_gallery_source = urllib.urlopen(playlist_gallery).read()
+        playlist_regex = 'data-context-item-id="(.+?)"'
+
+        playlist_IDs = re.findall(playlist_regex,playlist_gallery_source)
+        return playlist_IDs;
+
+def getVideos(playlist_IDs):
+        '''
+        Takes a list of playlist IDs and forms playlist URLs
+        Scrapes the playlist page for video IDs, and form video URLs
+        Scrapes each video for the high res image, and adds the image URL to a list
+
+        '''
+        for i in playlist_IDs:
+
+                playlist_source = urllib.urlopen('http://www.youtube.com/playlist?list='+i+'').read()
+                playlist_title_regex = '<h2 class="epic-nav-item-heading">(.+?)</h2>'
+                video_ID_regex = 'data-video-ids="(.+?)"'
+
+                playlist_title_raw = str(re.findall(playlist_title_regex, playlist_source))
+                video_IDs = re.findall(video_ID_regex,playlist_source)
+     
+                imagelist = []
+                linklist = []
+
+                for v in video_IDs:
+                        video_page = urllib.urlopen('http://www.youtube.com/watch?v='+v+'')
+                        video_page_source = video_page.read()
+                        image_URL_regex = 'http://i1.ytimg.com/vi/?[a-zA-Z0-9_-]*/maxresdefault.jpg'
+                        imagelist.append(re.findall(image_URL_regex,video_page_source))
+
+                for y in imagelist:
+                        #print y[0]
+                        #linklist.append(y[0])
+                #print linklist
+
+'''                        
 
 
-def getVideos(playlist):
-	for i in playlist:
-		playlist_source = urllib.urlopen('http://www.youtube.com/playlist?list='+i+'').read()
-		video_ID_regex = 'data-video-ids="(.+?)"'
-		get_video_ID = re.findall(video_ID_regex,playlist_source)
-		imagelist = []
-		linklist = []
-
-		for v in get_video_ID:
-			video_page = urllib.urlopen('http://www.youtube.com/watch?v='+v+'')
-			video_page_source = video_page.read()
-			image_URL_regex = 'http://i1.ytimg.com/vi/?[a-zA-Z0-9_-]*/maxresdefault.jpg'
-			imagelist.append(re.findall(image_URL_regex,video_page_source))
-
-		#print len(imagelist)
-
-		for y in imagelist:
-			print y[0]
-			#linklist.append(y[0])
-
-		#print linklist
-'''			
+                for x in linklist:
+                        split = urlparse.urlsplit(x)
+                        name1 = split.path.split("/vi")[-1]
+                        name2 = name1.replace("maxresdefault", "", 2)
+                        save_name = name2.replace("/", "", 2)
+                        urllib.urlretrieve(''+x+'','img/'+save_name+'')
+'''              
 
 
-		for x in linklist:
-			split = urlparse.urlsplit(x)
-			name1 = split.path.split("/vi")[-1]
-			name2 = name1.replace("maxresdefault", "", 2)
-			save_name = name2.replace("/", "", 2)
-			urllib.urlretrieve(''+x+'','img/'+save_name+'')
-'''		
-getVideos(getPlaylists('http://www.youtube.com/user/MrSuicideSheep/videos?view=1'))
+youtubeuser = raw_input('Type the channel name and press Enter: --> ')
+channel_link = 'http://www.youtube.com/user/'+ youtubeuser +'/'
+
+if not doesExist(channel_link):
+        print "Doesn't look like a channel."
+else:
+        print "\n","Looks good! - Checking for images now!","\n"
+        getVideos(getPlaylists(channel_link))
 
 
 '''
